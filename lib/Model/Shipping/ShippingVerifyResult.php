9fb56a8d512458b35655ba51da7b0783c2702fb5
svc-developer <svc.developer@avalara.com>
2022-04-26 01:38:11 +0000

    2.4.30

--- lib/Model/Shipping/ShippingVerifyResult.php
+++ lib/Model/Shipping/ShippingVerifyResult.php
@@ -28,7 +28,7 @@
  * @author     Jonathan Wenger <jonathan.wenger@avalara.com>
  * @copyright  2004-2022 Avalara, Inc.
  * @license    https://www.apache.org/licenses/LICENSE-2.0
- * @version    2.4.29
+ * @version    2.4.30
  * @link       https://github.com/avadev/AvaTax-REST-V3-PHP-SDK
 
  */
